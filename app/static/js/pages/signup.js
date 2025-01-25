defab06025504559cc1686f02aefd1697b1363f0
TaeryungIm <77139152+TaeryungIm@users.noreply.github.com>
2025-01-26 03:29:25 +0900

    1.26 first commit
    회원가입 창을 데모버전을 참고해서 도입함
    아직 뭔가 디자인이 이상하긴 한데 일단 그대로 두기로

--- app/static/js/pages/signup.js
+++ app/static/js/pages/signup.js
@@ -1,1 +1,26 @@
 // signup page에만 특화된 function 정의
+
+function postSignup(event) {
+    event.preventDefault();
+    // more specifics
+
+}
+
+function verifyEmail(event) {
+    event.preventDefault();
+    // more specifics
+
+}
+
+function reVerifyEmail(event) {
+    event.preventDefault();
+    // more specifics
+
+}
+
+function submitCode(event) {
+    event.preventDefault();
+    // more specifics
+
+}
+
---
+++ app/static/js/pages/withdrawal.js
@@ -0,0 +1,2 @@
+// cash withdrawal functionality에만 특화된 function 정의
+
