58df6e97df4642d6aa30e94394694bd4e629d4e5
TaeryungIm <77139152+TaeryungIm@users.noreply.github.com>
2025-02-13 08:22:41 +0900

    2.13 first commit
    faq-inquiry membership 부분 파일을 대규모로 디자인을 수정함

--- app/static/js/pages/faq-inquiry.js
+++ app/static/js/pages/faq-inquiry.js
@@ -2,23 +2,27 @@
 
 // Start on loading time
 document.addEventListener("DOMContentLoaded", function () {
-    const inquiryButton = document.getElementById('inquiryButton');
-    const inquiryContainer = document.getElementById('inquiryContainer');
-
-    const membershipButton = document.getElementById('membershipButton');
-    const membershipContainer = document.getElementById('membershipContainer');
+    const inquiryButton = document.getElementById('inquiry-button');
+    const membershipButton = document.getElementById('membership-button');
     const overlay = document.getElementById('overlay');
 
     inquiryButton.addEventListener('click', () => {
-        inquiryContainer.classList.add('open');
+        document.getElementById('inquiry-side').classList.add('open');
+        overlay.classList.add('show');
+    });
+
+    membershipButton.addEventListener('click', () => {
+        document.getElementById('membership-side').classList.add('open');
         overlay.classList.add('show');
     });
 
     overlay.addEventListener('click', () => {
-        inquiryContainer.classList.remove('open');
+        document.querySelectorAll('.side-container').forEach(container => container.classList.remove('open'));
         overlay.classList.remove('show');
+        document.getElementById('membership-description').textContent = "멤버쉽을 선택하면 설명이 여기에 나타납니다.";
     });
-
-
-
 });
+
+function showDescription(text) {
+    document.getElementById('membership-description').textContent = text;
+}
